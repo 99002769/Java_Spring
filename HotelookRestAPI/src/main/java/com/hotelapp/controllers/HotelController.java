0d33e31de2b7eb90df919882299c28f673cf2212
99002769 <72495916+99002769@users.noreply.github.com>
2020-10-22 18:47:38 +0530

    Add files via upload

---
+++ HotelookRestAPI/src/main/java/com/hotelapp/controllers/HotelController.java
@@ -0,0 +1,41 @@
+package com.hotelapp.controllers;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.hotelapp.model.Hotel;
+import com.hotelapp.service.HotelService;
+
+
+@RestController
+public class HotelController {
+	
+	@Autowired
+	HotelService hotelService;
+	@GetMapping("/hotels")
+	public List<Hotel> showAllHotels()
+	{
+		return hotelService.getAllHotels();
+	}
+	
+	@GetMapping("/hotels-by-Name/{Name}")
+	public List<Hotel> getByHotelName(@PathVariable("Name")String Name)
+	{
+		return hotelService.getByHotelName(Name);
+	}
+	
+	@GetMapping("/hotels-by-city/{city}")
+	public List<Hotel> getByCity(@PathVariable("city")String city)
+	{
+		return hotelService.getBycity(city);
+	}
+	@GetMapping("/hotels-by-id/{id}")
+	public Hotel getById(@PathVariable("id")int Id)
+	{
+		return hotelService. getById(Id);
+	}
+}
---
+++ HotelookRestAPI/src/main/java/com/hotelapp/service/HotelServiceImpl.java
@@ -0,0 +1,42 @@
+package com.hotelapp.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.hotelapp.dao.HotelDAO;
+import com.hotelapp.dao.HotelDAOImpl;
+import com.hotelapp.model.Hotel;
+
+@Service
+public class HotelServiceImpl implements HotelService {
+
+	@Autowired
+	HotelDAO hotelDAO;
+	@Override
+	public List<Hotel> getAllHotels() {
+		// TODO Auto-generated method stub
+		return hotelDAO.getAllHotels();
+	}
+
+	@Override
+	public List<Hotel> getByHotelName(String Name) {
+		// TODO Auto-generated method stub
+		return hotelDAO.getByHotelName(Name);
+	}
+
+	@Override
+	public List<Hotel> getBycity(String city) {
+		// TODO Auto-generated method stub
+		return hotelDAO.getBycity(city);
+		
+	}
+
+	@Override
+	public Hotel getById(int Id) {
+		// TODO Auto-generated method stub
+		return hotelDAO.getById(Id);
+	}
+
+}
